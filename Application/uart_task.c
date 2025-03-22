6124d2dd82dce44e1229560debf43db8052c1309
Yu Chen <1092508752@qq.com>
2025-03-22 15:42:45 +0800

    version1.2 更新项目框架

--- Application/uart_task.c
+++ Application/uart_task.c
@@ -6,8 +6,8 @@
 #include "all.h"
 void UART6_Task(void *argument){
     while(1){
-//        printf("UART2:%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d\n\r",pid_angle.kp,pid_angle.output,Target_Angle,motorA.speed,angle_Car_total,angle_speed,Key1);
+        printf("UART2:%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d\n\r",pid_angle.kp,pid_angle.output,Target_Angle,motorA.speed,angle_Car_total,angle_speed,Key1);
         vTaskDelay(55);
-        printf("Angle:%.2f\r\n",92.33);
+//        printf("Angle:%.2f\r\n",92.33);
     }
 }
